ba37d0268219071ad5e0b6ac21e055b1531dda53
Tristan Winters <poweredbynomad@gmail.com>
2016-03-19 20:40:34 +0100

    fizzbuzz using while loop

--- fizzbuzz2/fizzbuzz2.rb
+++ fizzbuzz2/fizzbuzz2.rb
@@ -4,5 +4,9 @@ while num < 101
     num += 1
     if num % 5 == 0 && num % 3 == 0
         puts "fizzbuzz"
+    elsif num % 3 == 0 
+        puts "fizz"
+    elsif num % 5 == 0 
+        puts "buzz"
     end
 end
