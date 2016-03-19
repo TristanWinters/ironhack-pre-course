9c5fee8af5cb6c62b49bbe2a8df7633102eb951a
Tristan Winters <poweredbynomad@gmail.com>
2016-03-19 20:54:56 +0100

    fizzbuzz iteration 2 final commit

--- fizzbuzz2/fizzbuzz2.rb
+++ fizzbuzz2/fizzbuzz2.rb
@@ -4,9 +4,10 @@ while num < 101
     num += 1
     if num % 5 == 0 && num % 3 == 0
         puts "fizzbuzz"
-    elsif num % 3 == 0 
-        puts "fizz"
     elsif num % 5 == 0 
         puts "buzz"
+    elsif num % 3 == 0 
+        puts "fizz"
+    else puts num 
     end
 end
