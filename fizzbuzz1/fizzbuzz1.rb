5bc4b53d2783564f3ca88db5b2b391e04bb818bb
Tristan Winters <poweredbynomad@gmail.com>
2016-03-19 17:21:32 +0100

    commit for first version of fizzbuzz

--- fizzbuzz1/fizzbuzz1.rb
+++ fizzbuzz1/fizzbuzz1.rb
@@ -1,3 +1,11 @@
 for num in 1..100
     puts num
+    if num % 5 == 0 && num % 3 == 0
+        puts "fizzbuzz"
+    elsif num % 5 == 0
+        puts "buzz" 
+    elsif num % 3 == 0
+        puts "fizz"
+    end
 end
+
