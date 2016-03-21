2a79868cfc7d38f6f7d0b4a0ecd530bb24446409
Tristan Winters <poweredbynomad@gmail.com>
2016-03-21 17:07:05 +0100

    fizzbuzz bang using for loop

--- fizzbuzz3/fizzbuzz3.rb
+++ fizzbuzz3/fizzbuzz3.rb
@@ -1,14 +1,26 @@
-num = 1 
-while num <= 100
-    puts num
-    num += 1
+# num = 1 
+# while num <= 100
+#     num += 1
+#     if num % 5 == 0 && num % 3 == 0
+#         puts "fizzbuzz"
+#     elsif num % 5 == 0 
+#         puts "buzz"
+#     elsif num % 3 == 0 
+#         puts "fizz"
+#     elsif num.to_s[0] == '1'
+#         puts "bang"
+#     else puts num 
+#     end
+# end
+
+for num in 1..100 do 
     if num % 5 == 0 && num % 3 == 0
         puts "fizzbuzz"
     elsif num % 5 == 0 
         puts "buzz"
     elsif num % 3 == 0 
         puts "fizz"
-    elsif num == 1
+    elsif num.to_s[0] == '1'
         puts "bang"
     else puts num 
     end
