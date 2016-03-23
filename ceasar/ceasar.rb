2231f90d9496aa88f0a8af5f2ba0d3478fb58162
Tristan Winters <poweredbynomad@gmail.com>
2016-03-23 17:08:58 +0100

    final version of ceasars cipher

--- ceasar/ceasar.rb
+++ ceasar/ceasar.rb
@@ -1,25 +1,30 @@
 require "pry"
 
-def solve_cipher(input) 
+def solve_cipher(input, shift_size) 
     message = input.split(//)
-    # message.each do |n|
-    #     puts n.chars
-    #     puts n.ord  
-    #  #.map(&:ord)
-    # end
 
     shifted_string = []
 
     message.each do |n| 
-        num_string = n.ord
-        num_string += -1
-        changed_string = num_string.chr
-        shifted_string<<changed_string  
+        str_to_num = n.ord
+        if str_to_num != 32  
+            str_to_num += shift_size  
+            if str_to_num < 97 
+                str_to_num += 26
+            end
+            if str_to_num > 122
+                str_to_num -= 26
+            end
+        end
+         
+        num_to_str = str_to_num.chr
+        shifted_string<<num_to_str  
     end
+
     
-    puts shifted_string
+    puts shifted_string.join
 
 
 end
 
-solve_cipher("ifmmp")
+solve_cipher("p| uhdo qdph lv grqdog gxfn", -3)
